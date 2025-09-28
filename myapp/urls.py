dd1502043baec53934fe957bddf22d39269c8aa9
dieu-black-hat <149935362+dieu-black-hat@users.noreply.github.com>
2025-09-28 10:08:27 +0200

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Root URL
-    path('room/', views.room, name='room'),  # Room URL
+    path('', views.index, name='index'),
+    path('room/', views.room, name='room'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('dash/', views.dash, name='dash'),
@@ -12,5 +12,9 @@ urlpatterns = [
     path('generatecode/', views.generatecode, name='generatecode'),
     path('generate-aruco/', views.generate_aruco_codes, name='generate_aruco'),
     path('scan_aruco/', views.scan_aruco, name='scan_aruco'),
-    path('video_feed/<int:camera_index>/', views.video_feed, name='video_feed'),  # ✅ added
+    path('video_feed/<int:camera_index>/', views.video_feed, name='video_feed'),
 ]
+
+# ✅ Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
